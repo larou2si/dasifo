5a320393ac74902c99b12dd3741f3704916c1de6
MLaroussi <MMLaroussi@itssglobal.com>
2022-01-16 20:15:44 +0100

    adding a scraping script

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # DASIFO
 
-We are working on this project, It is a documentation of different tools needed while working in Data Science!
+We are working on this project, It is a documentation of different tools needed while working in Data Science project!
 and we ll enrich it by some use cases that ll simplify and accelerate working by those technologies...
 We are looking to create a python library that will regroups:
- - all kind of Databases connections (SQL, NOSQL).
+ - all kind of Databases connections (SQL, NOSQL) and queries examples.
  - it provides some tutorials of APIs: for example conneting to google Drive, cloud....
  - machine learning!
  - deep learning!
@@ -19,6 +19,12 @@ Make sure you are on the right python's envirement variable.
  To install it locally, run this line in your terminal
     cd dasifo && python3 setup.py sdist bdist_wheel && pip3 install -e .
 ```
+
+## Uninstall our library 'dasifo'
+```
+pip3 uninstall dasifo
+```
+
 ## Databases installation
 ```
 Mysql: https://dev.mysql.com/doc/refman/5.7/en/installing.html
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='dasifo',
-    version='0.0.2',
+    version='0.0.3',
     author="larou2si",
     author_email="mohamed.laroussi.1@esprit.tn",
     description="This new Library is 'DAta Science In Full Options = dasifo': ...",
@@ -17,15 +17,15 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    #package_dir={"": "dasifo"},
+    packages=setuptools.find_packages(),
     python_requires='>=3.7',
-    install_requires=['numpy', 'scrapeasy', 'mysql-connector-python', 'psycopg2', 'postgis'],
+    install_requires=['numpy', 'scrapeasy', 'mysql-connector-python', 'psycopg2', 'postgis',
+                      'requests', 'beautifulsoup4'],
 )
 
 # to install this package in your python envirement:
-#       python3 setup.py sdist bdist_wheel
-#       pip3 install -e .
+#       python3 setup.py sdist bdist_wheel && pip3 install -e .
 # to upload your LIB to pypi
-#       python setup.py sdist
+#       python3 setup.py sdist
 #       twine upload dist/*
